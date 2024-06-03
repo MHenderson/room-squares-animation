5d9ec33060cc1df1de177411b126f5dbd3cf484a
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-06-03 08:18:36 +0000

    Consequences of native pipe.

--- R/plot_partial_room_square.R
+++ R/plot_partial_room_square.R
@@ -1,8 +1,8 @@
 plot_partial_room_square <- function(R) {
   ggplot(R, aes(col, row)) +
-    geom_tile(aes(fill = n_Pe)) +
-    geom_segment(data = grid_lines(n-1, n-1), aes(x = x, y = y, xend = xend, yend = yend), size = .1) +
-    geom_text(data = . %>% filter(!is.na(first)), aes(label = paste(first, second, sep = ",")), size = 2) +
+    geom_tile(data = R |> filter(is.na(first)), aes(fill = n_Pe)) +
+    geom_segment(data = grid_lines(n - 1, n - 1), aes(x = x, y = y, xend = xend, yend = yend), linewidth = .1) +
+    geom_text(data = R |> filter(!is.na(first)), aes(label = paste(first, second, sep = ",")), size = 2) +
     scale_y_reverse() +
     scale_fill_gradient(limits = c(0, choose(n, 2)), low = "white", high = "blue") +
     coord_fixed() + 
