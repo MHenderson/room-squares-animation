0ee8ebacd39c872418e66aee7267f50260ddf749
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-06-05 07:47:42 +0000

    Remove the R animation script.

--- R/animation.R
+++
@@ -1,8 +0,0 @@
-library(here)
-library(magick)
-
-imgs <- image_read(list.files(here("plot"), full.names = TRUE))
-
-animation <- image_animate(image_scale(imgs, "600x600"), fps = 4, dispose = "previous")
-
-image_write(animation, "gif/out.gif")
